48f78626377964dfb78a01d25c15cbc338d4183a
ToddHicks <72574563+ToddHicks@users.noreply.github.com>
2025-02-19 20:02:42 -0600

    Delete tic-tac-toe/src/components/ui directory

--- tic-tac-toe/src/components/ui/Card.js
+++
@@ -1,1 +0,0 @@
-export function Card({ children }) { return (<div className='p-4 border rounded-lg shadow-md bg-white'>{children}</div>); } export function CardContent({ children }) { return (<div className='p-2'>{children}</div>); }
