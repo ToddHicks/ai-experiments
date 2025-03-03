d4aac4207b012b023fe2da16014a77e0dd653ff9
ToddHicks <72574563+ToddHicks@users.noreply.github.com>
2025-03-02 20:29:00 -0600

    Add files via upload
    Decreased randomness.

--- connect4/connect4-server-postgres.py
+++ connect4/connect4-server-postgres.py
@@ -22,7 +22,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 alpha = 0.8 # Learns over experiences.
 gamma = 0.9 # Value on future rewards
-epsilon = 0.101 # Randomness
+epsilon = 0.01 # Randomness
 turn_bonus = 1.5 # Bonus for longer turns 
 
 games = {}
