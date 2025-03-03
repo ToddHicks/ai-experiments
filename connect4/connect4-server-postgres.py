bd7266173f69a713199c47cd5fefd36bbb7b5460
ToddHicks <72574563+ToddHicks@users.noreply.github.com>
2025-03-02 19:53:02 -0600

    Add files via upload
    Increased randomness and lowered rewards.

--- connect4/connect4-server-postgres.py
+++ connect4/connect4-server-postgres.py
@@ -22,8 +22,8 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 alpha = 0.8 # Learns over experiences.
 gamma = 0.9 # Value on future rewards
-epsilon = 0.001 # Randomness
-turn_bonus = 2.5 # Bonus for longer turns 
+epsilon = 0.101 # Randomness
+turn_bonus = 1.5 # Bonus for longer turns 
 
 games = {}
 games_lock = threading.Lock()
